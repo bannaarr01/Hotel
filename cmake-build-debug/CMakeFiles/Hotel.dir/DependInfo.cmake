aa69a05750102543b535cd7e896d8e40deea1606
bannaarr01 <tbannaarr@gmail.com>
2021-12-31 23:57:31 +0800

    initial commit

---
+++ cmake-build-debug/CMakeFiles/Hotel.dir/DependInfo.cmake
@@ -0,0 +1,60 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/josh/CLionProjects/Hotel/guestManager/GuestManager.cpp" "/Users/josh/CLionProjects/Hotel/cmake-build-debug/CMakeFiles/Hotel.dir/guestManager/GuestManager.cpp.o"
+  "/Users/josh/CLionProjects/Hotel/guestManager/guest/CreditCardDetails.cpp" "/Users/josh/CLionProjects/Hotel/cmake-build-debug/CMakeFiles/Hotel.dir/guestManager/guest/CreditCardDetails.cpp.o"
+  "/Users/josh/CLionProjects/Hotel/guestManager/guest/Guest.cpp" "/Users/josh/CLionProjects/Hotel/cmake-build-debug/CMakeFiles/Hotel.dir/guestManager/guest/Guest.cpp.o"
+  "/Users/josh/CLionProjects/Hotel/guestManager/guest/IdDetails.cpp" "/Users/josh/CLionProjects/Hotel/cmake-build-debug/CMakeFiles/Hotel.dir/guestManager/guest/IdDetails.cpp.o"
+  "/Users/josh/CLionProjects/Hotel/hotelUtil/HotelUtil.cpp" "/Users/josh/CLionProjects/Hotel/cmake-build-debug/CMakeFiles/Hotel.dir/hotelUtil/HotelUtil.cpp.o"
+  "/Users/josh/CLionProjects/Hotel/interface/IPrintable.cpp" "/Users/josh/CLionProjects/Hotel/cmake-build-debug/CMakeFiles/Hotel.dir/interface/IPrintable.cpp.o"
+  "/Users/josh/CLionProjects/Hotel/interface/IVisitor.cpp" "/Users/josh/CLionProjects/Hotel/cmake-build-debug/CMakeFiles/Hotel.dir/interface/IVisitor.cpp.o"
+  "/Users/josh/CLionProjects/Hotel/ioManager/IoManager.cpp" "/Users/josh/CLionProjects/Hotel/cmake-build-debug/CMakeFiles/Hotel.dir/ioManager/IoManager.cpp.o"
+  "/Users/josh/CLionProjects/Hotel/main.cpp" "/Users/josh/CLionProjects/Hotel/cmake-build-debug/CMakeFiles/Hotel.dir/main.cpp.o"
+  "/Users/josh/CLionProjects/Hotel/overallManager/OverallManager.cpp" "/Users/josh/CLionProjects/Hotel/cmake-build-debug/CMakeFiles/Hotel.dir/overallManager/OverallManager.cpp.o"
+  "/Users/josh/CLionProjects/Hotel/paymentManager/PaymentManager.cpp" "/Users/josh/CLionProjects/Hotel/cmake-build-debug/CMakeFiles/Hotel.dir/paymentManager/PaymentManager.cpp.o"
+  "/Users/josh/CLionProjects/Hotel/paymentManager/Promotion.cpp" "/Users/josh/CLionProjects/Hotel/cmake-build-debug/CMakeFiles/Hotel.dir/paymentManager/Promotion.cpp.o"
+  "/Users/josh/CLionProjects/Hotel/paymentManager/payment/PaymentDetails.cpp" "/Users/josh/CLionProjects/Hotel/cmake-build-debug/CMakeFiles/Hotel.dir/paymentManager/payment/PaymentDetails.cpp.o"
+  "/Users/josh/CLionProjects/Hotel/reservationManager/ReservationManager.cpp" "/Users/josh/CLionProjects/Hotel/cmake-build-debug/CMakeFiles/Hotel.dir/reservationManager/ReservationManager.cpp.o"
+  "/Users/josh/CLionProjects/Hotel/reservationManager/reservation/Reservation.cpp" "/Users/josh/CLionProjects/Hotel/cmake-build-debug/CMakeFiles/Hotel.dir/reservationManager/reservation/Reservation.cpp.o"
+  "/Users/josh/CLionProjects/Hotel/roomManager/RoomManager.cpp" "/Users/josh/CLionProjects/Hotel/cmake-build-debug/CMakeFiles/Hotel.dir/roomManager/RoomManager.cpp.o"
+  "/Users/josh/CLionProjects/Hotel/roomManager/room/Room.cpp" "/Users/josh/CLionProjects/Hotel/cmake-build-debug/CMakeFiles/Hotel.dir/roomManager/room/Room.cpp.o"
+  "/Users/josh/CLionProjects/Hotel/roomManager/roomType/RoomType.cpp" "/Users/josh/CLionProjects/Hotel/cmake-build-debug/CMakeFiles/Hotel.dir/roomManager/roomType/RoomType.cpp.o"
+  "/Users/josh/CLionProjects/Hotel/roomManager/roomType/RoomTypeDeluxe.cpp" "/Users/josh/CLionProjects/Hotel/cmake-build-debug/CMakeFiles/Hotel.dir/roomManager/roomType/RoomTypeDeluxe.cpp.o"
+  "/Users/josh/CLionProjects/Hotel/roomManager/roomType/RoomTypeDouble.cpp" "/Users/josh/CLionProjects/Hotel/cmake-build-debug/CMakeFiles/Hotel.dir/roomManager/roomType/RoomTypeDouble.cpp.o"
+  "/Users/josh/CLionProjects/Hotel/roomManager/roomType/RoomTypeSingle.cpp" "/Users/josh/CLionProjects/Hotel/cmake-build-debug/CMakeFiles/Hotel.dir/roomManager/roomType/RoomTypeSingle.cpp.o"
+  "/Users/josh/CLionProjects/Hotel/roomManager/roomType/RoomTypeVip.cpp" "/Users/josh/CLionProjects/Hotel/cmake-build-debug/CMakeFiles/Hotel.dir/roomManager/roomType/RoomTypeVip.cpp.o"
+  "/Users/josh/CLionProjects/Hotel/ui/UI.cpp" "/Users/josh/CLionProjects/Hotel/cmake-build-debug/CMakeFiles/Hotel.dir/ui/UI.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BOOST_ALL_NO_LIB"
+  "BOOST_FILESYSTEM_DYN_LINK"
+  "BOOST_SYSTEM_DYN_LINK"
+  "JSON_DIAGNOSTICS=0"
+  "JSON_USE_IMPLICIT_CONVERSIONS=1"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
